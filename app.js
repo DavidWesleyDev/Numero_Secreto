0d4169c28b1fc169bbcda045b4a3d88b6427932b
David Wesley <davidwesleydesenvolvedor@gmail.com>
2025-04-07 00:41:00 -0300

    atualização no JavaScript
    foi adicionado novas funcionalidades no javascript, como o alert, prompt, variáveis , condicional (IF)...

---
+++ app.js
@@ -0,0 +1,7 @@
+alert ('Boas vindas ao Jogo do Numero Secreto!');
+let numeroSecreto = 7;
+let chute = prompt('Escolha um número de 1 a 10');
+
+if (numeroSecreto == chute) {
+    console.log('Isso ai meu chapa quente! você descobriu o número Secreto (7)');
+}
