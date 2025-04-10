b057eae473a3eca6a1b558ef09c4bf640762f420
David Wesley <davidwesleydesenvolvedor@gmail.com>
2025-04-09 22:31:06 -0300

    Atualização do jogo Numero secreto

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 alert ('Boas vindas ao Jogo do Numero Secreto!');
-let numeroSecreto = 7;
+let numeroSecreto = 8;
+console.log(numeroSecreto);
 let chute = prompt('Escolha um número de 1 a 10');
 
-if (numeroSecreto == chute) {
-    console.log('Isso ai meu chapa quente! você descobriu o número Secreto (7)');
+//Se o chute for igual ao numero secreto
+if (chute == numeroSecreto) {
+    alert(`Isso ai meu chapa quente! você descobriu o número Secreto ${numeroSecreto} `);
+    console.log(`Isso ai meu chapa quente! você descobriu o ${númeroSecreto}`);
+}else{
+    alert('Você errou :(')
+    console.log('Errou!');
 }
